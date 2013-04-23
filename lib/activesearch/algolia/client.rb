80e4e99e9def94d17aba633c803a5f220ffd0253
Rodrigo Alvarez <papipo@gmail.com>
2013-04-23 12:37:49 +0200

    small fix

--- lib/activesearch/algolia/client.rb
+++ lib/activesearch/algolia/client.rb
@@ -1,34 +1,36 @@
 require "httparty"
 require "active_support/core_ext/object/to_json"
 
-module ActiveSearch::Algolia
-  class Client
-    include HTTParty
-    
-    def self.configure(api_key, app_id, index = "activesearch")
-      base_uri "https://apieu1.algolia.com/1/indexes/#{index}"
-      headers({
-        "X-Algolia-API-Key" => api_key,
-        "X-Algolia-Application-Id" => app_id,
-        "Content-Type" => "application/json; charset=utf-8"
-      })
-    end
+module ActiveSearch
+  module Algolia
+    class Client
+      include HTTParty
+  
+      def self.configure(api_key, app_id, index = "activesearch")
+        base_uri "https://apieu1.algolia.com/1/indexes/#{index}"
+        headers({
+          "X-Algolia-API-Key" => api_key,
+          "X-Algolia-Application-Id" => app_id,
+          "Content-Type" => "application/json; charset=utf-8"
+        })
+      end
 
-    def delete_index
-      self.class.delete("")
-    end
+      def delete_index
+        self.class.delete("")
+      end
 
-    def delete(id)
-      return false if id.nil?
-      self.class.delete("/#{id}")
-    end
+      def delete(id)
+        return false if id.nil?
+        self.class.delete("/#{id}")
+      end
 
-    def save(id, object)
-      self.class.put("/#{id}", body: object.to_json)
-    end
+      def save(id, object)
+        self.class.put("/#{id}", body: object.to_json)
+      end
 
-    def query(text, extras = {})
-      self.class.get("", query: extras.merge!(query: text))
+      def query(text, extras = {})
+        self.class.get("", query: extras.merge!(query: text))
+      end
     end
   end
 end
